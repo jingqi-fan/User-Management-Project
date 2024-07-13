daf49bb3aecb82d509746c8c96972c1dd3670607
Jingqi Fan <jingqi_fan@163.com>
2024-07-13 13:50:44 +0800

    Add files via upload

---
+++ back/webBackEnd1/target/classes/schama/user.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE `user`  (
+     `id` int(11) NOT NULL AUTO_INCREMENT,
+     `login_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '登录名',
+     `user_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+     `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+     `sex` varchar(8) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
+     `email` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+     `address` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
+     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+insert into user(id, login_name, user_name, password, sex, email, address)
+    VALUE (1,'a','a','a','a','zhumianyi@163.com','a')
